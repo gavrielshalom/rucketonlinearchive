c3fa217074012066fafc060a5a9c34892ef56d1a
barcafatih <60362437+barcafatih@users.noreply.github.com>
2021-04-07 20:34:45 +0300

    sus mk

---
+++ App.js
@@ -0,0 +1,73 @@
+
+function showVideo1(){
+    var x = document.getElementById("div1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo2(){
+    var x = document.getElementById("div2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo3(){
+    var x = document.getElementById("div3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo4(){
+    var x = document.getElementById("div4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo5(){
+    var x = document.getElementById("div5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo6(){
+    var x = document.getElementById("div6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo7(){
+    var x = document.getElementById("div7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo8(){
+    var x = document.getElementById("div8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showVideo9(){
+    var x = document.getElementById("div9");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
